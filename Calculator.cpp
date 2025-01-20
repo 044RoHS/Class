1cd29497df8b484b851611755cb4ca98b498400f
Artyom <kolmazeff@yandex.ru>
2025-01-20 11:27:25 +0300

    Second commit

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,7 +4,7 @@
 
 class Calculator 
 {
-private:
+public:
     double num1 = 0;
     double num2 = 0;
 public: 
@@ -38,15 +38,27 @@ public:
        std::cout << "num2 / num1 = " << num2 / num1 << std::endl;
         return 0;
     }
-    bool set_num1(double num1) 
+    bool set_num1(double *num1) 
     {
-        num1 == this->num1 ? true : false;
-        return 0;
+        if (*num1 != this->num1) 
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
-    bool set_num2(double num2) 
+    bool set_num2(double *num2) 
     {
-        num2 == this-> num2? true : false;
-        return 0;
+        if (*num2 != this->num2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        };
     }
 
 };
@@ -58,13 +70,14 @@ int main()
     double FirstNumber;
     double SecondNumber;
     Calculator cal;
-   while (true)
-   {
-        std::cout << "Введите num1: ";
-        std::cin >> FirstNumber;
-        std::cout << "Введите num2: ";
-        std::cin >> SecondNumber;
-        if (cal.set_num1(FirstNumber) == true && cal.set_num2(SecondNumber) == true) 
+    std::cout << "Введите num1: ";
+    std::cin >> FirstNumber;
+    std::cout << "Введите num2: ";
+    std::cin >> SecondNumber;
+    while (true)
+    {
+
+        if (cal.set_num1(&FirstNumber) == true && cal.set_num2(&SecondNumber) == true)
         {
             cal.add(FirstNumber, SecondNumber);
             cal.multiply(FirstNumber, SecondNumber);
@@ -72,21 +85,38 @@ int main()
             cal.subtract_2_1(FirstNumber, SecondNumber);
             cal.divide_1_2(FirstNumber, SecondNumber);
             cal.divide_2_1(FirstNumber, SecondNumber);
+            std::cout << "Введите num1: ";
+            std::cin >> FirstNumber;
+            std::cout << "Введите num2: ";
+            std::cin >> SecondNumber;
         }
-        else if (cal.set_num1(FirstNumber) == false && cal.set_num2(SecondNumber) == true)
+        else if (cal.set_num1(&FirstNumber) == false && cal.set_num2(&SecondNumber) == true)
         {
-            std::cout << "Неверный ввод! ";
+            std::cout << "Неверный ввод! " << std::endl;
             std::cout << "Введите num1: ";
             std::cin >> FirstNumber;
             
-        }
-        else if (cal.set_num1(FirstNumber) == true && cal.set_num2(SecondNumber) == false)
-        {
-            std::cout << "Неверный ввод! ";
-            std::cout << "Введите num1: ";
-            std::cin >> FirstNumber;
 
         }
+        else if (cal.set_num1(&FirstNumber) == true && cal.set_num2(&SecondNumber) == false)
+        {
+            std::cout << "Неверный ввод! " << std::endl;
+            std::cout << "Введите num2: ";
+            std::cin >> SecondNumber;
+            
+        }
+        else 
+        {
+            std::cout << "Неверный ввод! " << std::endl;
+            std::cout << "Введите num1: ";
+            std::cin >> FirstNumber;
+            std::cout << "Введите num2: ";
+            std::cin >> SecondNumber;
+
+        }
+    
+       
+       
        
 
 
